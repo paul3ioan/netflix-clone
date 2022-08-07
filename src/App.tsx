f4551a249724edae12f782e13d094b7cac34b7ae
Paul <cirstean.paul.ioan@gmail.com>
2022-08-07 20:12:35 +0300

    Add boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <p className="text-3xl font-bold underline text-red-600">Edit and save to reload.</p>
+    </div>
+  );
+}
+
+export default App;
